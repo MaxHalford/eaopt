1074cd407296676fcaa38a90e792a4e5f699592b
Max Halford <maxhalford25@gmail.com>
2016-03-28 16:37:39 +0200

    Added mutation tests

---
+++ mutation_test.go
@@ -0,0 +1,73 @@
+package gago
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+var potentMutators = []Mutator{
+	FloatNormal{Rate: 1.0, Std: 1},
+	Splice{Rate: 1.0},
+	Permute{Rate: 1.0},
+}
+
+var impotentMutators = []Mutator{
+	FloatNormal{Rate: 0.0, Std: 1},
+	Splice{Rate: 0.0},
+	Permute{Rate: 0.0},
+}
+
+func TestPotentMutators(t *testing.T) {
+	var (
+		nbGenes   = 4
+		source    = rand.NewSource(time.Now().UnixNano())
+		generator = rand.New(source)
+		indi      = Individual{make([]interface{}, nbGenes), 0.0}
+		init      = FloatUniform{-5.0, 5.0}
+	)
+	init.apply(&indi, generator)
+	var genome = make([]interface{}, len(indi.Genome))
+	copy(genome, indi.Genome)
+	// Probability to mutate is equal to 1
+	for _, mutator := range potentMutators {
+		mutator.apply(&indi, generator)
+		// Check the genome is still the same size
+		if len(indi.Genome) != nbGenes {
+			t.Error("Genome size was changed after mutation")
+		}
+		// Check the genome has changed
+		for i := range indi.Genome {
+			if indi.Genome[i] == genome[i] {
+				t.Error("Genome was not modified after potent mutation")
+			}
+		}
+	}
+}
+
+func TestImpotentMutators(t *testing.T) {
+	var (
+		nbGenes   = 4
+		source    = rand.NewSource(time.Now().UnixNano())
+		generator = rand.New(source)
+		indi      = Individual{make([]interface{}, nbGenes), 0.0}
+		init      = FloatUniform{-5.0, 5.0}
+	)
+	init.apply(&indi, generator)
+	var genome = make([]interface{}, len(indi.Genome))
+	copy(genome, indi.Genome)
+	// Probability to mutate is equal to 0
+	for _, mutator := range impotentMutators {
+		mutator.apply(&indi, generator)
+		// Check the genome is still the same size
+		if len(indi.Genome) != nbGenes {
+			t.Error("Genome size was changed after mutation")
+		}
+		// Check the genome has changed
+		for i := range indi.Genome {
+			if indi.Genome[i] != genome[i] {
+				t.Error("Genome was modified after impotent mutation")
+			}
+		}
+	}
+}
