078264c28b42bebac623d7d18164418b1aaa1cec
Max Halford <maxhalford25@gmail.com>
2016-03-29 08:57:11 +0200

    More tests
    Former-commit-id: 66c12d66acac1f9c768c84bc7a63371aff8168d9

---
+++ migration_test.go
@@ -0,0 +1,22 @@
+package gago
+
+import "testing"
+
+var demeSizes = []int{1, 2, 4}
+
+func TestShuffle(t *testing.T) {
+	for _, size := range demeSizes {
+		// Instantiate a population
+		var pop = Float
+		pop.Migrator = Shuffle{}
+		pop.NbDemes = size
+		// Apply the migration method
+		pop.migrate()
+		// Check the deme sizes haven't changed
+		for _, deme := range pop.Demes {
+			if len(deme.Individuals) != pop.NbIndividuals {
+				t.Error("Shuffle migration changed the deme sizes")
+			}
+		}
+	}
+}
