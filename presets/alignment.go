b758a2bcff29afcbe73e635f02f53031e3a31a90
Max Halford <maxhalford25@gmail.com>
2016-05-16 23:05:25 +0200

    Fix tests
    Former-commit-id: 01409dd9a752c1ff696a0533e24c1a88dfc14df0

--- presets/alignment.go
+++ presets/alignment.go
@@ -6,28 +6,28 @@ import "github.com/MaxHalford/gago"
 // function. The output will be a genome of a certain length with genes
 // belonging to a corpus of elements.
 func Alignment(length int, corpus []string, distance func([]string) float64) gago.GA {
-	return gago.GA{
-		NbPopulations: 2,
-		NbIndividuals: 30,
-		NbGenes:       length,
-		NbParents:     6,
-		Ff: gago.StringFunction{
-			Image: distance,
-		},
-		Initializer: gago.ISUniform{
-			Corpus: corpus,
-		},
-		Selector: gago.STournament{
-			NbParticipants: 3,
-		},
-		Crossover: gago.CPoint{
-			NbPoints: 2,
-		},
-		Mutator: gago.MutPermute{
-			Max: 3,
-		},
-		MutRate:      0.5,
-		Migrator:     gago.MigShuffle{},
-		MigFrequency: 10,
-	}
+    return gago.GA{
+        NbPopulations: 2,
+        NbIndividuals: 30,
+        NbGenes:       length,
+        NbParents:     6,
+        Ff: gago.StringFunction{
+            Image: distance,
+        },
+        Initializer: gago.ISUniform{
+            Corpus: corpus,
+        },
+        Selector: gago.STournament{
+            NbParticipants: 3,
+        },
+        Crossover: gago.CPoint{
+            NbPoints: 2,
+        },
+        Mutator: gago.MutPermute{
+            Max: 3,
+        },
+        MutRate:      0.5,
+        Migrator:     gago.MigShuffle{},
+        MigFrequency: 10,
+    }
 }
