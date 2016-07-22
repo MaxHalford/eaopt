2a8f17f947f7539491042d7584f372234320b021
MaxHalford <maxhalford25@gmail.com>
2016-07-22 11:44:38 +0200

    Add clustering
    Former-commit-id: f706d85aca5c6f8dc3042ae434b45f16b89c2130

--- clustering_test.go
+++ clustering_test.go
@@ -6,33 +6,8 @@ import (
 	"time"
 )
 
-func TestClusteringMerge(t *testing.T) {
+func TestClustering(t *testing.T) {
 	var (
-		nbIndividuals = []int{1, 2, 3}
-		nbClusters    = []int{1, 2, 3}
-		src           = rand.NewSource(time.Now().UnixNano())
-		rng           = rand.New(src)
-	)
-	for _, nbi := range nbIndividuals {
-		for _, nbc := range nbClusters {
-			var clusters = make(Clusters, nbc)
-			// Fill the clusters with individuals
-			for i := 0; i < nbc; i++ {
-				clusters[i] = makeIndividuals(nbi, 1, rng)
-			}
-			// Merge
-			var indis = clusters.merge()
-			// Check the clusters of individuals
-			if len(indis) != nbi*nbc {
-				t.Error("Merge didn't work properly")
-			}
-		}
-	}
-}
-
-func TestCluFitness(t *testing.T) {
-	var (
-		clu = CluFitness{}
 		N   = []int{7, 10}
 		K   = []int{2, 3}
 		src = rand.NewSource(time.Now().UnixNano())
@@ -43,14 +18,39 @@ func TestCluFitness(t *testing.T) {
 			var (
 				m        = n/k + 1
 				indis    = makeIndividuals(n, 1, rng)
-				clusters = clu.Apply(indis, k)
+				pop      = Population{Individuals: indis}
+				clusters = pop.cluster(k)
 			)
 			// Check the cluster sizes are equal to min(n-i, m) where i is a
 			// multiple of m
 			for i, cluster := range clusters {
-				if len(cluster) != min(n-i*m, m) {
-					t.Error("CluFitness didn't split individuals correctly")
+				if len(cluster.Individuals) != min(n-i*m, m) {
+					t.Error("Clustering didn't split individuals correctly")
 				}
+			}
+		}
+	}
+}
+
+func TestClusteringMerge(t *testing.T) {
+	var (
+		nbIndividuals = []int{1, 2, 3}
+		nbClusters    = []int{1, 2, 3}
+		src           = rand.NewSource(time.Now().UnixNano())
+		rng           = rand.New(src)
+	)
+	for _, nbi := range nbIndividuals {
+		for _, nbc := range nbClusters {
+			var clusters = make(Populations, nbc)
+			// Fill the clusters with individuals
+			for i := 0; i < nbc; i++ {
+				clusters[i] = Population{Individuals: makeIndividuals(nbi, 1, rng)}
+			}
+			// Merge
+			var indis = clusters.merge()
+			// Check the clusters of individuals
+			if len(indis) != nbi*nbc {
+				t.Error("Merge didn't work properly")
 			}
 		}
 	}
---
+++ docs/clustering.md
@@ -0,0 +1,6 @@
+Clusters are a partitioning of individuals into smaller groups of similar
+individuals. In other words a clusters is a slice of slices containing
+individuals. The similarity depends on a metric defined elsewhere. The
+purpose of a partinioning individuals is to apply genetic operators inside a
+cluster. In biological terms this encourages "incest" and maintains isolated
+species.
