2413889a2780069808cc17844d0a06a792b355e8
MaxHalford <maxhalford25@gmail.com>
2016-10-23 17:49:54 +0200

    gago2 -> gago
    Former-commit-id: 4af94e2db01f31516503eeb924f4a3244db13757

--- preset/simulated_annealing.go
+++ preset/simulated_annealing.go
@@ -1,16 +1,16 @@
 package preset
 
-import "github.com/MaxHalford/gago2"
+import "github.com/MaxHalford/gago"
 
 // SimAnn is a GA instance that mimicks a basic simulated annealing procedure.
-func SimAnn(GenomeMaker gago2.GenomeMaker) gago2.GA {
-	var ga = gago2.GA{
+func SimAnn(GenomeMaker gago.GenomeMaker) gago.GA {
+	var ga = gago.GA{
 		MakeGenome: GenomeMaker,
-		Topology: gago2.Topology{
+		Topology: gago.Topology{
 			NbrPopulations: 1,
 			NbrIndividuals: 1,
 		},
-		Model: gago2.ModSimAnn{
+		Model: gago.ModSimAnn{
 			T:     100,  // Starting temperature
 			Tmin:  1,    // Stopping temperature
 			Alpha: 0.99, // Decrease rate per iteration
