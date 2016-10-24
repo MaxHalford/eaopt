5bc30cc56e9fa86b0b99d1263d3a5b110dcf90e7
MaxHalford <maxhalford25@gmail.com>
2016-10-24 20:29:48 +0200

    wip
    Former-commit-id: 3b19d7206d0f27b2fe1993dd21cb54241496b003

--- mutate/splice.go
+++ mutate/splice.go
@@ -4,53 +4,30 @@ import "math/rand"
 
 // Splice splits a genome in 3 and glues the parts back together in another
 // order.
-func Splice(genome []interface{}, rng *rand.Rand) {
+func splice(genome Castable, rng *rand.Rand) []interface{} {
+	var slice = genome.Cast()
 	// Choose where to start and end the splice
 	var (
-		end   = rng.Intn(len(genome)-1) + 1
+		end   = rng.Intn(len(slice)-1) + 1
 		start = rng.Intn(end)
 	)
 	// Split the genome into two
 	var inner = make([]interface{}, end-start)
-	copy(inner, genome[start:end])
-	var outer = append(genome[:start], genome[end:]...)
+	copy(inner, slice[start:end])
+	var outer = append(slice[:start], slice[end:]...)
 	// Choose where to insert the splice
 	var insert = rng.Intn(len(outer))
 	// Splice and insert
-	genome = append(
+	slice = append(
 		outer[:insert],
 		append(
 			inner,
 			outer[insert:]...,
 		)...,
 	)
+	return slice
 }
 
-// Convenience types for common cases
-
-// Splice a []int.
-func (ints IntSlice) Splice(rng *rand.Rand) {
-	var genome = make([]interface{}, len(ints))
-	for i, v := range ints {
-		genome[i] = v
-	}
-	Splice(genome, rng)
-}
-
-// Splice a []float64.
-func (floats Float64Slice) Splice(rng *rand.Rand) {
-	var genome = make([]interface{}, len(floats))
-	for i, v := range floats {
-		genome[i] = v
-	}
-	Splice(genome, rng)
-}
-
-// Splice a []string.
-func (strings StringSlice) Splice(rng *rand.Rand) {
-	var genome = make([]interface{}, len(strings))
-	for i, v := range strings {
-		genome[i] = v
-	}
-	Splice(genome, rng)
+func SpliceFloat64s(floats []float64, rng *rand.Rand) []interface{} {
+	return splice(Float64Slice(floats), rng)
 }
