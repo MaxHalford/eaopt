36aecdcc062d2a24b16a3afba076bf27644246e5
MaxHalford <maxhalford25@gmail.com>
2016-12-09 17:38:08 +0100

    Implement roulette wheel selection
    Former-commit-id: 11cda5d3c9e4014dc617048fde5513c0f1c252b8

--- selection_test.go
+++ selection_test.go
@@ -50,3 +50,54 @@ func TestSelTournament(t *testing.T) {
 		t.Error("Full SelTournament didn't select the best individual")
 	}
 }
+
+func TestGetWeights(t *testing.T) {
+	var testCases = []struct {
+		fitnesses []float64
+		weights   []float64
+	}{
+		{[]float64{-10, -8, -5}, []float64{5 / 8, 1, 1}},
+		{[]float64{-2, 0, 2, 3}, []float64{5 / 9, 8 / 9, 1, 1}},
+		{[]float64{1, 2, 3, 4}, []float64{0.1, 0.3, 0.6, 1}},
+	}
+	for _, test := range testCases {
+		var weights = getWeights(test.fitnesses)
+		for i := range weights {
+			if weights[i] != test.weights[i] {
+				t.Error("getWeights didn't work as expected")
+			}
+		}
+	}
+}
+
+func TestSpin(t *testing.T) {
+	var testCases = []struct {
+		value float64
+		wheel []float64
+		index int
+	}{
+		{0.1, []float64{0.3, 0.7, 1}, 0},
+		{0.3, []float64{0.3, 0.7, 1}, 1},
+		{0.8, []float64{0.3, 0.7, 1}, 2},
+	}
+	for _, test := range testCases {
+		var index = spin(test.value, test.wheel)
+		if index != test.index {
+			t.Error("spin didn't work as expected")
+		}
+	}
+}
+
+func TestSelRoulette(t *testing.T) {
+	var (
+		rng   = makeRandomNumberGenerator()
+		indis = makeIndividuals(30, MakeVector, rng)
+		sel   = SelTournament{len(indis)}
+	)
+	for _, n := range []int{0, 1, 10, 30} {
+		var selected, _ = sel.Apply(n, indis, rng)
+		if len(selected) != n {
+			t.Error("SelRoulette didn't select the right number of individuals")
+		}
+	}
+}
