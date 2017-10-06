1a5ecddf79bb1be1963c05f7d7dabee3da5a6f16
Max Halford <maxhalford25@gmail.com>
2017-10-06 19:14:42 +0200

    Add benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,8 +1,76 @@
 package gago
 
-import "testing"
+import (
+	"runtime"
+	"testing"
+)
 
-func BenchmarkEnhance(b *testing.B) {
+func BenchmarkEnhance1Pop(b *testing.B) {
+	ga = GA{
+		NewGenome: NewVector,
+		NPops:     1,
+		PopSize:   50,
+		Model: ModGenerational{
+			Selector: SelTournament{
+				NContestants: 3,
+			},
+			MutRate: 0.5,
+		},
+	}
+	ga.Initialize()
+	for i := 0; i < b.N; i++ {
+		ga.Enhance()
+	}
+}
+
+func BenchmarkEnhance2Pops(b *testing.B) {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	ga = GA{
+		NewGenome: NewVector,
+		NPops:     2,
+		PopSize:   50,
+		Model: ModGenerational{
+			Selector: SelTournament{
+				NContestants: 3,
+			},
+			MutRate: 0.5,
+		},
+	}
+	ga.Initialize()
+	for i := 0; i < b.N; i++ {
+		ga.Enhance()
+	}
+}
+
+func BenchmarkEnhance3Pops(b *testing.B) {
+	ga = GA{
+		NewGenome: NewVector,
+		NPops:     3,
+		PopSize:   50,
+		Model: ModGenerational{
+			Selector: SelTournament{
+				NContestants: 3,
+			},
+			MutRate: 0.5,
+		},
+	}
+	ga.Initialize()
+	for i := 0; i < b.N; i++ {
+		ga.Enhance()
+	}
+}
+func BenchmarkEnhance4Pops(b *testing.B) {
+	ga = GA{
+		NewGenome: NewVector,
+		NPops:     4,
+		PopSize:   50,
+		Model: ModGenerational{
+			Selector: SelTournament{
+				NContestants: 3,
+			},
+			MutRate: 0.5,
+		},
+	}
 	ga.Initialize()
 	for i := 0; i < b.N; i++ {
 		ga.Enhance()
